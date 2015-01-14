4c45da51333f7bf4d0a6b65e4e013f969f1db4ee
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-01-14 15:38:39 -0300

    Added Dropbox storage support.

--- libraries/storages.rb
+++ libraries/storages.rb
@@ -134,6 +134,17 @@ module Mo
         option "keep", :number, 5
       end
 
+      class Dropbox < Default
+        option "api_key", :string
+        option "api_secret", :string
+        option "cache_path", :string, ".cache"
+        option "access_type", :symbol, :app_folder
+        option "path", :string, "/backups"
+        option "keep", :number, 5
+        option "chunk_size", :number
+        option "max_retries", :number
+        option "retry_waitsec", :number
+      end
 
       class Sftp < Default
         option "username", :string, 'user'
