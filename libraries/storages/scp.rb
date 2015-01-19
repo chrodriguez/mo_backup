959ecb4e0d13a54064ee29079f18c1ce52c37780
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-01-19 18:10:44 -0300

    Added SCP storage.

---
+++ libraries/storages/scp.rb
@@ -0,0 +1,17 @@
+require_relative 'default'
+
+module Mo
+  module Backup
+    module Storages
+      class Scp < Default
+        option "username", :string, 'user'
+        option "password", :string, 'pass'
+        option "ip", :string
+        option "port", :number, 22
+        option "path", :string, "~"
+        option "keep", :number, 5
+        storage_id "SCP"
+      end
+    end
+  end
+end
