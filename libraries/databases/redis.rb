a9c7b9b09c1ee00f04f24c8ae5e89dc63626af98
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-02-05 10:51:31 -0300

    Added Redis database support.

---
+++ libraries/databases/redis.rb
@@ -0,0 +1,18 @@
+require_relative 'default'
+
+module Mo
+  module Backup
+    module Databases
+      class Redis < Default
+        option "mode", :symbol, "copy"
+        option "host", :string, "localhost"
+        option "port", :number, 6379
+        option "socket", :string
+        option "password", :string
+        option "rdb_path", :string
+        option "additional_options", :array
+        option "invoke_save", :boolean, false
+      end
+    end
+  end
+end
