5f48c2499ff4966fa1dd1952bdf8cf804bd3055c
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-03-04 15:17:20 -0300

    Unify creteria
    data recived by mo_backup_generate_model and mo_backup_schedule_job must
    be treated the same way

--- libraries/mixin_model.rb
+++ libraries/mixin_model.rb
@@ -3,10 +3,10 @@ require 'etc'
 def mo_backup_generate_model(data)
 
   # Generate empty config.rb if it does not exist.
-  file ::File.join(::Dir.home(data["user"]), data["backup"]["dir"] || node["mo_backup"]["dir"], "config.rb") do
+  file "backuo config file #{data["id"]}" do
+    lazy { ::File.join(::Dir.home(data['backup']["user"]), data["backup"]["dir"] || node["mo_backup"]["dir"], "config.rb")}
     content "# Backup v4.x Configuration"
-    owner data["user"]
-    group get_group(data["user"])
+    owner data['backup']["user"]
     mode '0755'
     action :create
   end
@@ -14,20 +14,20 @@ def mo_backup_generate_model(data)
   # If any of the data bags does not exist the following lines would fail. 
   # Check how to ask if a data bag is defined.
   storages = get_storages(data["backup"]["storages_databag"] || node["mo_backup"]["storages_databag"], data["backup"]["storages"])
-  databases = get_databases(data["databases"].slice(data['backup']['databases']))
+  databases = get_databases(mo_backup_slice(data["databases"], data['backup']['databases']))
   mail_config = get_mail_config(data["backup"]["mail_databag"] || node["mo_backup"]["mail_databag"], data["backup"]["mail"])
   syncers = get_syncers(data["backup"]["syncers_databag"] || node["mo_backup"]["syncers_databag"], data["backup"]["syncers"])
 
-  directory ::File.join(::Dir.home(data["user"]), data["backup"]["models_dir"] || node["mo_backup"]["models_dir"]) do
-    owner data["user"]
-    group get_group(data["user"])
+  directory "backup model directory for #{data["id"]}" do
+    path lazy {::File.join(::Dir.home(data['backup']["user"]), data["backup"]["models_dir"] || node["mo_backup"]["models_dir"])}
+    owner data['backup']["user"]
     action :create
     recursive true
   end
 
-  template ::File.join(::Dir.home(data["user"]), data["backup"]["models_dir"] || node["mo_backup"]["models_dir"], "#{data["id"]}_#{node.chef_environment}.rb") do
-    owner data["user"]
-    group get_group(data["user"])
+  template "backup model template #{data["id"]}" do
+    source lazy {::File.join(::Dir.home(data['backup']["user"]), data["backup"]["models_dir"] || node["mo_backup"]["models_dir"], "#{data["id"]}_#{node.chef_environment}.rb")}
+    owner data['backup']["user"]
     source "model.rb.erb"
     cookbook "mo_backup"
     variables( :app => data, :name => "#{data["id"]}_#{node.chef_environment}", :description => data["description"],
@@ -36,17 +36,16 @@ def mo_backup_generate_model(data)
   end
 end
 
-def mo_backup_schedule_job(data, action="create")
-
+def mo_backup_schedule_job(data)
   cron data["id"] do
-    minute data["schedule"]["minute"]    || "0"
-    hour data["schedule"]["hour"]        || "2"
-    day data["schedule"]["day"]          || "*"
-    month data["schedule"]["month"]      || "*"
-    weekday data["schedule"]["weekday"]  || "*"
-    user data["user"]                     || "root"
+    minute data["backup"]["schedule"]["minute"]    || "0"
+    hour data["backup"]["schedule"]["hour"]        || "2"
+    day data["backup"]["schedule"]["day"]          || "*"
+    month data["backup"]["schedule"]["month"]      || "*"
+    weekday data["backup"]["schedule"]["weekday"]  || "*"
+    user data['backup']["user"]                    || "root"
     command "/opt/rbenv/shims/backup perform --trigger #{data["id"]}_#{node.chef_environment}"
-    action action.to_sym
+    action (data["backup"]['enabled'].nil? || data["backup"]['enabled']) ? :create : :delete
   end
 end
 
@@ -80,6 +79,7 @@ def get_mail_config(mail_databag, mail)
   Chef::Mixin::DeepMerge.deep_merge(mail, mail_config)
 end
 
-def get_group(user)
-  Etc.getgrgid(Etc.getpwnam(user).gid).name
+def mo_backup_slice(hash, keys)
+  keys.each_with_object(Hash.new) {|k,h| h[k] = hash[k] }
 end
+
